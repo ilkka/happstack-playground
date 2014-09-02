afd4890c4aea351aa38a9054be41c9f6023c68bf
Ilkka Laukkanen <ilkka@fastmail.fm>
2014-09-02 21:25:33 +0300

    Implement the rest of the methods
    Remove some stuff that lint complained about

--- Main.hs
+++ Main.hs
@@ -17,19 +17,19 @@ main = serve Nothing myApp
 
 myApp :: ServerPart Response
 myApp = msum
-        [ dir "echo"    $ echo
-        , dir "query"   $ queryParams
-        , dir "form"    $ formPage
-        , dir "fortune" $ fortune
-        , dir "files"   $ fileServing
-        , dir "upload"  $ upload
+        [ dir "echo"    echo
+        , dir "query"   queryParams
+        , dir "form"    formPage
+        , dir "fortune" fortune
+        , dir "files"   fileServing
+        , dir "upload"  upload
         , homePage
         ]
 
 template :: Text -> Html -> Response
 template title body = toResponse $
   H.html $ do
-    H.head $ do
+    H.head $
       H.title (toHtml title)
     H.body $ do
       body
@@ -59,7 +59,7 @@ queryParams =
   do mFoo <- optional $ lookText "foo"
      ok $ template "Query parameters" $ do
        p $ "foo is " >> toHtml (show mFoo)
-       p $ "change the URL param to get something else."
+       p "change the URL param to get something else."
 
 formPage :: ServerPart Response
 formPage = msum [viewForm, processForm]
@@ -69,7 +69,7 @@ formPage = msum [viewForm, processForm]
       do method GET
          ok $ template "form" $
            form ! action "/form" ! enctype "multipart/form-data" ! A.method "POST" $ do
-             label ! A.for "msg" $ "Say something clever"
+             label ! A.for "msg" $ "Say something clever: "
              input ! type_ "text" ! A.id "msg" ! name "msg"
              input ! type_ "submit" ! value "Say it!"
     processForm :: ServerPart Response
@@ -77,14 +77,52 @@ formPage = msum [viewForm, processForm]
       do method POST
          msg <- lookText "msg"
          ok $ template "form" $ do
-           H.p "You said:"
+           H.p "You said: "
            H.p (toHtml msg)
 
 fortune :: ServerPart Response
-fortune = ok $ template "(Fortune) cookies" $ do H.h1 "(Fortune) cookies"
+fortune = msum [viewFortune, updateFortune]
+  where
+    viewFortune :: ServerPart Response
+    viewFortune =
+      do method GET
+         mMemory <- optional $ lookCookieValue "fortune"
+         let memory = fromMaybe "Your future is filled with web programming." mMemory
+         ok $ template "Fortunes" $ do
+           H.p "Your fortune sez: "
+           H.p (toHtml memory)
+           form ! action "/fortune" ! enctype "multipart/form-data" ! A.method "POST" $ do
+             label ! A.for "fortune" $ "Change your fortune: "
+             input ! type_ "text" ! A.id "fortune" ! name "new_fortune"
+             input ! type_ "submit" ! value "Change it!"
+    updateFortune :: ServerPart Response
+    updateFortune =
+      do method POST
+         newFortune <- lookText "new_fortune"
+         addCookies [(Session, mkCookie "fortune" (unpack newFortune))]
+         seeOther ("/fortune" :: String) (toResponse ())
 
 fileServing :: ServerPart Response
-fileServing = ok $ template "File serving" $ do H.h1 "File serving"
+fileServing =
+    serveDirectory EnableBrowsing ["index.html"] "."
 
 upload :: ServerPart Response
-upload = ok $ template "Uploads" $ do H.h1 "Uploads"
+upload = msum [uploadForm, handleUpload]
+  where
+    uploadForm :: ServerPart Response
+    uploadForm =
+      do method GET
+         ok $ template "upload" $ do
+           form ! action "/upload" ! enctype "multipart/form-data" ! A.method "POST" $ do
+             label ! A.for "file" $ "Select file: "
+             input ! type_ "file" ! A.id "file" ! name "file"
+             input ! type_ "submit" ! value "Upload it!"
+    handleUpload :: ServerPart Response
+    handleUpload =
+      do method POST
+         (tmpFile, uploadName, contentType) <- lookFile "file"
+         ok $ template "file uploaded" $ do
+           H.p (toHtml $ "temporary file: " ++ tmpFile)
+           H.p (toHtml $ "uploaded name: " ++ uploadName)
+           H.p (toHtml $ "content type: " ++ show contentType)
+
