52990c27491c87940826a7ce3e6fa5c9493eab0c
Ilkka Laukkanen <ilkka@fastmail.fm>
2014-09-07 15:29:12 +0300

    Updated to Happstack Server

--- Main.hs
+++ Main.hs
@@ -3,17 +3,18 @@
 module Main where
 
 import Control.Applicative ((<$>), optional)
+import Control.Monad (msum)
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Data.Text.Lazy (unpack)
-import Happstack.Lite
+import Happstack.Server
 import Text.Blaze.Html5 ((!), Html, a, form, input, p, toHtml, label)
 import Text.Blaze.Html5.Attributes (action, enctype, href, name, size, type_, value)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
 main :: IO ()
-main = serve Nothing myApp
+main = simpleHTTP nullConf myApp
 
 myApp :: ServerPart Response
 myApp = msum
